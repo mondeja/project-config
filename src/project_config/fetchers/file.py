b156d97178a46fd70b0b95b23093b6f0c68b6a54
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-06-23 22:16:26 +0200

    open files with utf-8 encoding

--- src/project_config/fetchers/file.py
+++ src/project_config/fetchers/file.py
@@ -10,7 +10,7 @@ def fetch(url_parts: urllib.parse.SplitResult) -> str:
     """Fetch a file, just read it from filesystem."""
     url = url_parts.geturl()
     try:
-        with open(os.path.expanduser(url)) as f:
+        with open(os.path.expanduser(url), encoding="utf-8") as f:
             return f.read()
     except FileNotFoundError:
         raise FetchError(f"'{url}' file not found")
