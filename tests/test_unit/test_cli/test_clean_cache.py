0fa8f638e8f069c3be6e6cd6c5d3d6bc9bc2b30d
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-06-28 10:57:41 +0200

    Fix test

--- tests/test_unit/test_cli/test_clean_cache.py
+++ tests/test_unit/test_cli/test_clean_cache.py
@@ -1,10 +1,17 @@
 import os
 import shutil
+import sys
+
+import pytest
 
 from project_config.__main__ import run
 from project_config.cache import Cache
 
 
+@pytest.mark.skipif(
+    "win" in sys.platform,
+    reason="Failing on Windows due to PermissionError",
+)
 def test_clean_cache(capsys, tmp_path):
     cache_dir = Cache.get_directory()
     temp_cache_dir = str(tmp_path / "cache")
