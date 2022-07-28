bd26b7503f7781f12d3f14abfbb3f14297bcd0fb
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-07-28 19:05:08 +0200

    Allow to install using NPM (#133)
    * Add installation using NPM
    
    * Remove file
    
    * Improvements in test
    
    * Setup NodeJS in CI
    
    * Only execute test in Unix
    
    * Improve documentation
    
    * Fix error
    
    * Fix error
    
    * Remove 3.9 on Windows and MacOS from CI
    
    * Only setup Node.js when running in Unix on CI
    
    * Update poetry in CI

---
+++ tests/test_acceptance/test_npm_package.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import subprocess
+
+from testing_helpers import mark_end2end, mark_unix_only, rootdir
+
+
+@mark_end2end
+@mark_unix_only
+def test_npm_pack_and_install(tmp_path):
+    packer_dir = tmp_path / "packer"
+    user_dir = tmp_path / "user"
+    user_dir.mkdir()
+
+    package_src_dir = os.path.join(rootdir, "contrib", "npm")
+    shutil.copytree(package_src_dir, packer_dir)
+
+    proc = subprocess.run(
+        ["npm", "pack"],
+        check=True,
+        cwd=packer_dir,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    assert proc.returncode == 0
+
+    tgz_filename = list(
+        filter(
+            lambda name: name.startswith("python-project-config"),
+            os.listdir(packer_dir),
+        ),
+    )[0]
+    tgz_path = packer_dir / tgz_filename
+    assert os.path.isfile(tgz_path)
+
+    proc = subprocess.run(
+        ["npm", "init", "-y"],
+        check=True,
+        cwd=user_dir,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    assert proc.returncode == 0
+
+    proc = subprocess.run(
+        ["npm", "i", tgz_path],
+        check=True,
+        cwd=user_dir,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+    package_json_file = user_dir / "package.json"
+    assert (
+        '"python-project-config": "file:../packer/python-project-config-'
+    ) in package_json_file.read_text()
+
+    assert proc.returncode == 0
--- tests/testing_helpers/__init__.py
+++ tests/testing_helpers/__init__.py
@@ -22,7 +22,7 @@ mark_end2end = pytest.mark.skipif(
 )
 
 mark_unix_only = pytest.mark.skipif(
-    "sys" in sys.platform,
+    "win" in sys.platform and sys.platform != "darwin",
     reason="Windows does not support this test",
 )
 
