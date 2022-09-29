e98153113f081d571e263c70f7eb28ac61ddf7ee
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-09-29 17:09:50 +0200

    Add test to check that SPDX versions of examples are updated (#149)
    Add test to update that SPDX versions of example are updated

---
+++ tests/test_docs/test_examples_data.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+from testing_helpers import mark_end2end, rootdir
+
+from project_config.fetchers.github import get_latest_release_tags
+
+
+@mark_end2end
+def test_spdx_license_list_data_version_updated():
+    license_list_data_regex = r"spdx/license-list-data@(v[^/]+)/"
+
+    latest_spdx_license_version = get_latest_release_tags(
+        "spdx",
+        "license-list-data",
+    )[0]
+
+    # iterate over all files localed in '{rootdir}/examples' directory
+    # and check if they contain a valid SPDX license list data version:
+    for root, _, files in os.walk(os.path.join(rootdir, "examples")):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, encoding="utf-8") as f:
+                file_content = f.read()
+            match = re.search(license_list_data_regex, file_content)
+            if match:
+                version = match.group(1)
+                assert version == latest_spdx_license_version, (
+                    f"File {file_path} contains an outdated SPDX"
+                    f" license list data version: {version}. "
+                    "Please update it to the latest version:"
+                    f" {latest_spdx_license_version}."
+                )
